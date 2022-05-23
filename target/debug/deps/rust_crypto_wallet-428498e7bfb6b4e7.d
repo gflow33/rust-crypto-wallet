515d63ffb34fb1cf0896286f8d3193b26b48b771
kaioh33 <gupajibril@gmail.com>
2022-05-23 12:07:36 +0100

    Add first commit

---
+++ target/debug/deps/rust_crypto_wallet-428498e7bfb6b4e7.d
@@ -0,0 +1,5 @@
+/home/kaioh/rust-crypto-wallet/target/debug/deps/rust_crypto_wallet-428498e7bfb6b4e7.rmeta: src/main.rs
+
+/home/kaioh/rust-crypto-wallet/target/debug/deps/rust_crypto_wallet-428498e7bfb6b4e7.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rust_crypto_wallet-d4cac9680e737a1c.d
@@ -0,0 +1,5 @@
+/home/kaioh/rust-crypto-wallet/target/debug/deps/rust_crypto_wallet-d4cac9680e737a1c.rmeta: src/main.rs
+
+/home/kaioh/rust-crypto-wallet/target/debug/deps/rust_crypto_wallet-d4cac9680e737a1c.d: src/main.rs
+
+src/main.rs:
